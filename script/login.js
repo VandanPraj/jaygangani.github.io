783cd412758bfaa59cb07b18c0dbe2d3a61b3a49
jaygangani <jaygangani1212@gmail.com>
2020-09-09 17:35:58 +0530

    login page completed

--- script/login.js
+++ script/login.js
@@ -32,41 +32,19 @@ function canclelogin(){
     }
   }
 
-    var slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) { slideIndex = 1 }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += " active";
-      setTimeout(showSlides, 6000);
-    }
-
-
-    function contextmenu() {
-      document.addEventListener('contextmenu',
-        event => event.preventDefault());
-    }
+   
 
     var modal = document.getElementById("myModal");
-
     var btn = document.getElementById("myBtn");
-
-
     var span = document.getElementsByClassName("close")[0];
+    var forgotpass = document.getElementById("forgotpass");
 
     btn.onclick = function () {
       modal.style.display = "block";
+    }
+    forgotpass.onclick = function () {
+      location = "../index.html"
+
     }
 
     // When the user clicks on <span> (x), close the modal
